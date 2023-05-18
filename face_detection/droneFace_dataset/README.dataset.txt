363a95a4e40d413e4973d87d90d59d33df969d88
ChangHorng <changhorng05@gmail.com>
2023-05-18 13:12:40 +0800

    Add comment for face tracking code

--- face_detection/droneFace_dataset/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# DroneFace > 2023-04-25 3:52pm
-https://universe.roboflow.com/face-detection-and-recognition-dataset/droneface
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- face_tracking/real_time_face_tracking.py
+++ face_tracking/real_time_face_tracking.py
@@ -10,36 +10,36 @@ from detectron2.utils.visualizer import ColorMode, Visualizer
 
 from djitellopy import Tello
 
+
 # fbRange = [6200, 6800]
-fbRange = [4200,4800]
-
+fbRange = [4200, 4800]
 pid = [0.4, 0.4, 0]
-
 pError = 0
 
 
-def get_frame(drone: Tello, width, height):
-    img = drone.get_frame_read().frame
-    img_resize = cv2.resize(img, (width, height))
-    return img_resize
+def detectron2_face_detection_config_setup():
 
+    # Retrieve metadata of registered dataset
+    MetadataCatalog.get("droneFace_train_dataset").set(thing_classes="face")
 
-def face_detection_setup():
-    MetadataCatalog.get("df_train").set(thing_classes="face")
-
+    # Initialize config for real-time
     cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+
+    cfg.OUTPUT_DIR = '../face_detection/train/output'
     cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.85
-    cfg.MODEL.DEVICE = "cpu"
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9
+    # cfg.MODEL.DEVICE = 'cpu'                  # Uncomment this line if your device does not have a CUDA compatible GPU
 
+    # Set up default predictor
     predictor = DefaultPredictor(cfg)
 
     return predictor
 
 
 def face_detection(predictor, frame):
+
     outputs = predictor(frame)
     tensor = outputs["instances"]
     coordinates = tensor.pred_boxes.tensor
@@ -52,7 +52,7 @@ def face_detection(predictor, frame):
     )
 
     instances = outputs["instances"].to("cpu")
-    # instances.remove('pred_masks')
+    instances.remove('pred_masks')
     v = v.draw_instance_predictions(instances)
     result = v.get_image()[:, :, ::-1]
 
@@ -61,7 +61,6 @@ def face_detection(predictor, frame):
     myFaceListArea = []
     # x, y, w, h = coordinates[0][0].item(), coordinates[0][1].item(), coordinates[0][2].item(), coordinates[0][3].item()
     if len(coordinates) != 0:
-        print(coordinates)
         x, y, w, h = coordinates[0][0].item(), coordinates[0][1].item(), coordinates[0][2].item(), coordinates[0][3].item()
 
         cx = x + w // 2
@@ -78,10 +77,11 @@ def face_detection(predictor, frame):
         else:
             return result, [[0, 0], 0]
 
-    return result, [[0,0],0]
+    return result, [[0, 0], 0]
 
 
-def trackFace(info,w,pid, pError):
+def track_face(info, w, pid, pError):
+
     face_area = info[1]
     center_x, center_y = info[0]
     center_y = center_y - 20
@@ -119,9 +119,18 @@ def trackFace(info,w,pid, pError):
     return error
 
 
+def get_frame(drone, width, height):
+    img = drone.get_frame_read().frame
+    img_resize = cv2.resize(img, (width, height))
+
+    return img_resize
+
+
 if __name__ == "__main__":
 
-    predictor = face_detection_setup()
+    predictor = detectron2_face_detection_config_setup()
+
+    width, height = 640, 480
 
     tello = Tello()
     tello.connect()
@@ -130,33 +139,29 @@ if __name__ == "__main__":
     tello.takeoff()
     print(tello.get_battery())
 
-    exit = 0
-    width, height = 640, 480
     counter = 0
+    exit = 0
     while not exit:
-
         frame = get_frame(tello, width, height)
         # keep drone alive
 
-            # print(frame)
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #     tello.land()
         #     break
         # else:
             # tello.send_command_without_return('Stay_alive')
 
+        annotated_frame, info = face_detection(predictor, frame)
+        pError = track_face(info, width, pid, pError)
 
-        # annotated_frame, info = face_detection(predictor, frame)
-        # pError = trackFace(info,width,pid,pError)
-
-        cv2.imshow("Real-time Face Detection", frame)
+        cv2.imshow("Real-time Face Tracking", frame)
 
         # cv2.imshow("Real-time Face Detection", annotated_frame)
         cv2.waitKey(10)
             
-            # exit = int(input("Type 0 if you want to stop; else 1: "))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             tello.land()
             break
 
     tello.streamoff()
+    tello.end()
